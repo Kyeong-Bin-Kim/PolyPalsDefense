cde163a45393ded0d21a6d43d6d24e89ee3ff04b
김경빈 <rlarudqls316@naver.com>
2025-06-13 19:46:39 +0900

    20250613_사전설정

--- Source/PolyPalsDefense/PolyPalsDefense.Build.cs
+++ Source/PolyPalsDefense/PolyPalsDefense.Build.cs
@@ -28,7 +28,7 @@ public class PolyPalsDefense : ModuleRules
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NetCore", "NavigationSystem", "Niagara"  });
 
 		// Uncomment if you are using Slate UI
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
